1ab20a1dbacec3eb73b88f6cbd65f835f3e8ef03
Watney <1253349077@qq.com>
2024-12-21 19:28:52 +0800

    change

--- python/Net.py
+++ python/Net.py
@@ -156,28 +156,23 @@ class Bilinear_250(nn.Module):
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(32),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(p=0.5),
-
             # Second Convolutional Block
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(64),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(p=0.5),
 
             # Third Convolutional Block
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(p=0.5),
 
             # Fourth Convolutional Block
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(p=0.5),
 
             # Average Pooling
             nn.AvgPool2d(kernel_size=2, stride=2),
@@ -202,46 +197,200 @@ class Bilinear_250(nn.Module):
         x = torch.sigmoid(self.classifiers(x))
         return x.squeeze(1)
 
-
 class Cnn_150(nn.Module):
     def __init__(self):
         super(Cnn_150, self).__init__()
 
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=3], out_channels = 32, kernel_size = 3, padding = 0, stride = 1),
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=0, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
 
-    )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
 
-    self.layer2 = nn.Sequential(
-    nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=0),
-    nn.ReLU(),
-    nn.MaxPool2d(kernel_size=2, stride=2)
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
 
-)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(18496, 128)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(128, 1)
+        self.sigmoid = nn.Sigmoid()
 
-self.layer3 = nn.Sequential(
-nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=0),
-nn.ReLU(),
-nn.MaxPool2d(kernel_size=2, stride=2)
-)
-
-self.flatten = nn.Flatten()
-self.fc1 = nn.Linear(18496, 128)
-self.relu = nn.ReLU()
-self.dropout = nn.Dropout(0.5)
-self.fc2 = nn.Linear(128, 1)
-self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc2(x))
+        return x.squeeze(1)
 
 
-def forward(self, x):
-    x = self.layer1(x)
-    x = self.layer2(x)
-    x = self.layer3(x)
-    x = self.flatten(x)
-    x = self.fc1(x)
-    x = self.relu(x)
-    x = self.dropout(x)
-    x = torch.sigmoid(self.fc2(x))
-    return x.squeeze(1)
+class AlexNet_150(nn.Module):
+    def __init__(self, input_shape):
+        super(AlexNet_150, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=4, stride=1,
+                               padding=1)  # 修改padding为1以保持输出尺寸
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2)
+        # 计算全连接层的输入维度
+        self.fc1 = nn.Linear(64 * 17 * 17, 2048)  # 根据Keras模型的输出形状调整
+        self.fc2 = nn.Linear(2048, 1024)
+        self.fc3 = nn.Linear(1024, 1)
+        self.flatten = nn.Flatten()
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+        x = self.relu(self.conv3(x))
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc3(x))
+        return x.squeeze(1)
+
+
+class Dnn_150(nn.Module):
+    def __init__(self, input_shape):
+        super(Dnn_150, self).__init__()
+
+        # First Convolutional Block
+        self.conv1_1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.conv1_2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout1 = nn.Dropout(p=0.5)
+
+        # Second Convolutional Block
+        self.conv2_1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.conv2_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout2 = nn.Dropout(p=0.5)
+
+        # Third Convolutional Block
+        self.conv3_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv3_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout3 = nn.Dropout(p=0.5)
+
+        # Fourth Convolutional Block
+        self.conv4_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv4_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout4 = nn.Dropout(p=0.5)
+
+        # Average Pooling
+        self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        # Fully Connected Layers
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(128 * 4 * 4, 1024)  # Corrected input dimension
+        self.fc2 = nn.Linear(1024, 1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.conv1_1(x))
+        x = self.relu(self.conv1_2(x))
+        x = self.bn1(x)
+        x = self.pool1(x)
+        x = self.dropout1(x)
+
+        x = self.relu(self.conv2_1(x))
+        x = self.relu(self.conv2_2(x))
+        x = self.bn2(x)
+        x = self.pool2(x)
+        x = self.dropout2(x)
+
+        x = self.relu(self.conv3_1(x))
+        x = self.relu(self.conv3_2(x))
+        x = self.bn3(x)
+        x = self.pool3(x)
+        x = self.dropout3(x)
+
+        x = self.relu(self.conv4_1(x))
+        x = self.relu(self.conv4_2(x))
+        x = self.bn4(x)
+        x = self.pool4(x)
+        x = self.dropout4(x)
+
+        x = self.avg_pool(x)
+        x = self.flatten(x)
+        x = self.relu(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        return x.squeeze(1)
+
+
+class Bilinear_150(nn.Module):
+    def __init__(self):
+        super(Bilinear_150, self).__init__()
+
+        self.features = nn.Sequential(
+
+            # First Convolutional Block
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Second Convolutional Block
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Third Convolutional Block
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Fourth Convolutional Block
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Average Pooling
+            nn.AvgPool2d(kernel_size=2, stride=2),
+        )
+
+        self.classifiers = nn.Sequential(
+            nn.Linear(128 ** 2, 1),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        batch_size = x.size(0)
+
+        x = x.view(batch_size, 128, 4 ** 2)  ##
+
+        x = (torch.bmm(x, torch.transpose(x, 1, 2)) / 4 ** 2).view(batch_size, -1)  ##
+
+        x = torch.nn.functional.normalize(torch.sign(x) * torch.sqrt(torch.abs(x) + 1e-10))
+
+        # feature = feature.view(feature.size(0), -1)
+
+        x = torch.sigmoid(self.classifiers(x))
+        return x.squeeze(1)
