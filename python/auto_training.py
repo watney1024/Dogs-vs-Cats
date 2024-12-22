c21197c131ab72343b0974216e230e6b9afc4ca1
Watney <1253349077@qq.com>
2024-12-22 14:52:32 +0800

    change

--- python/auto_training.py
+++ python/auto_training.py
@@ -10,6 +10,8 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
+from Net import Cnn_250, AlexNet_250, Dnn_250, Bilinear_250, Cnn_150, AlexNet_150, Dnn_150, Bilinear_150
+
 # 配置 logging
 logging.basicConfig(filename='training.log',
                     level=logging.INFO,
@@ -22,8 +24,25 @@ parser = argparse.ArgumentParser(description='Training script with variable batc
 parser.add_argument('--batch_size', type=int, default=16, help='Batch size for training and validation')
 parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate for the optimizer')
 parser.add_argument('--epochs', type=int, default=1, help='Number of epochs to train')
-# parser.add_argument(('--model',type=))
+parser.add_argument('--input_shape', type=tuple, default=(3, 150, 150), help='Size of picture')
+parser.add_argument('--model', type=str, default='Cnn', help='Type of model')
 args = parser.parse_args()
+
+# 将命令行参数记录到日志
+logging.info("Command line arguments:")
+for arg_name in vars(args):
+    logging.info(f"{arg_name}: {getattr(args, arg_name)}")
+
+model_dict = {
+    'Cnn_250': Cnn_250,
+    'AlexNet_250': AlexNet_250,
+    'Dnn_250': Dnn_250,
+    'Bilinear_250': Bilinear_250,
+    'Cnn_150': Cnn_150,
+    'AlexNet_150': AlexNet_150,
+    'Dnn_150': Dnn_150,
+    'Bilinear_150': Bilinear_150
+}
 
 # 训练集目录
 TRAIN_DIRS = ['./dataset_torch/train1', './dataset_torch/train2', './dataset_torch/train3', './dataset_torch/train4',
@@ -34,15 +53,15 @@ VAL_DIRS = ['./dataset_torch/val1', './dataset_torch/val2', './dataset_torch/val
 
 # 定义归一化转换，将像素值归一化到 [-1, 1] 之间
 normalize = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-input_shape = (3, 150, 150)
+input_shape = args.input_shape
 train_transform = transforms.Compose([
-    transforms.Resize((150, 150)),
+    transforms.Resize((input_shape[1], input_shape[2])),
     transforms.ToTensor(),
     normalize  # 应用归一化
 ])
 
 val_transform = transforms.Compose([
-    transforms.Resize((150, 150)),
+    transforms.Resize((input_shape[1], input_shape[2])),
     transforms.ToTensor(),
     normalize  # 应用归一化
 ])
@@ -51,54 +70,6 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.manual_seed(3407)
 if device == 'cuda':
     torch.cuda.manual_seed_all(3407)
-
-
-class Bilinear(nn.Module):
-    def __init__(self, input_shape):
-        super(Bilinear, self).__init__()
-        self.input_shape = input_shape
-
-        self.features = nn.Sequential(
-            # First Convolutional Block
-            nn.Conv2d(in_channels=input_shape[0], out_channels=32, kernel_size=5, stride=1, padding=2),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            # Second Convolutional Block
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            # Third Convolutional Block
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            # Fourth Convolutional Block
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            # Average Pooling
-            nn.AvgPool2d(kernel_size=2, stride=2),
-        )
-
-        self.classifiers = nn.Sequential(
-            nn.Linear(128 ** 2, 1),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        batch_size = x.size(0)
-
-        x = x.view(batch_size, 128, 4 ** 2)
-
-        x = (torch.bmm(x, torch.transpose(x, 1, 2)) / 4 ** 2).view(batch_size, -1)
-
-        x = torch.nn.functional.normalize(torch.sign(x) * torch.sqrt(torch.abs(x) + 1e-10))
-
-        x = torch.sigmoid(self.classifiers(x))
-        return x.squeeze(1)
 
 
 # 定义训练函数
@@ -146,7 +117,7 @@ def val(dataloader, model, loss_fn):
 
 
 # 定义画图函数
-def matplot_loss(train_loss, val_loss):
+def matplot_loss(train_loss, val_loss, model_name, batch_size, epochs, input_shape):
     plt.plot(train_loss, label='train_loss')
     plt.plot(val_loss, label='val_loss')
     plt.legend(loc='best')
@@ -155,10 +126,12 @@ def matplot_loss(train_loss, val_loss):
     plt.ylim(bottom=0)  # 设置y轴的最小值为0
     plt.xlim(left=0)  # 设置x轴的最小值为0，如果epoch从1开始，可以去掉这行
     plt.title("loss")
+    file_name = f"{model_name}_batch_{batch_size}_epochs_{epochs}_input_{input_shape[1]}x{input_shape[2]}_loss.png"
+    plt.savefig(file_name)
     plt.show()
 
 
-def matplot_acc(train_acc, val_acc):
+def matplot_acc(train_acc, val_acc, model_name, batch_size, epochs, input_shape):
     plt.plot(train_acc, label='train_acc')
     plt.plot(val_acc, label='val_acc')
     plt.legend(loc='best')
@@ -167,6 +140,8 @@ def matplot_acc(train_acc, val_acc):
     plt.ylim(bottom=0)  # 设置y轴的最小值为0
     plt.xlim(left=0)  # 设置x轴的最小值为0，如果epoch从1开始，可以去掉这行
     plt.title("acc ")
+    file_name = f"{model_name}_batch_{batch_size}_epochs_{epochs}_input_{input_shape[1]}x{input_shape[2]}_acc.png"
+    plt.savefig(file_name)
     plt.show()
 
 
@@ -183,7 +158,8 @@ avg_acc_val = 0
 
 for i in range(5):
     logging.info(f"Fold {i + i}\n")
-    model = Bilinear(input_shape).to(device)
+    model_name = args.model
+    model = model_dict[args.model]().to(device)
     loss_fn = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
@@ -192,14 +168,14 @@ for i in range(5):
     train_dataset = ImageFolder(ROOT_TRAIN, transform=train_transform)
     val_dataset = ImageFolder(ROOT_TEST, transform=val_transform)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=0)
-    val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=True, num_workers=0)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=6)
+    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True, num_workers=6)
     loss_train = []
     acc_train = []
     loss_val = []
     acc_val = []
 
-    epoch = 1
+    epoch = args.epochs
     min_acc = 0
     best_epoch = 0
     for t in range(epoch):
@@ -225,7 +201,8 @@ for i in range(5):
 
             min_acc = val_acc
             best_epoch = t
-            torch.save(model.state_dict(), 'save_model/best_bilinear.pth')
+            file_name = f"{model_name}_batch_{args.batch_size}_epochs_{args.epochs}_input_{args.input_shape[1]}x{args.input_shape[2]}_best.pth"
+            torch.save(model.state_dict(), os.path.join(folder, file_name))
         # 保存最后一轮的权重文件
         # if t == epoch - 1:
         #     torch.save(model.state_dict(), 'save_model/last_bilinear.pth')
