8c029595bcfc1e60cdf9300208f1c9b9612e242d
root <root@NUC-ao-tan.evas.ai>
2025-01-02 15:34:28 +0800

    add quantize experiment

--- python/all.py
+++ python/all.py
@@ -1,10 +1,12 @@
 import os
 import time
+import psutil
 
 import matplotlib.pyplot as plt
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.quantization import get_default_qat_qconfig, prepare_qat, convert
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
@@ -44,52 +46,46 @@ class Bilinear(nn.Module):
     def __init__(self, input_shape):
         super(Bilinear, self).__init__()
         self.input_shape = input_shape
-
         self.features = nn.Sequential(
-
             # First Convolutional Block
             nn.Conv2d(in_channels=input_shape[0], out_channels=32, kernel_size=5, stride=1, padding=2),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(32),
             nn.MaxPool2d(kernel_size=2, stride=2),
-
             # Second Convolutional Block
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(64),
             nn.MaxPool2d(kernel_size=2, stride=2),
-
             # Third Convolutional Block
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(kernel_size=2, stride=2),
-
             # Fourth Convolutional Block
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(kernel_size=2, stride=2),
-
             # Average Pooling
             nn.AvgPool2d(kernel_size=2, stride=2),
         )
-
         self.classifiers = nn.Sequential(
             nn.Linear(128 ** 2, 1),
         )
-
+        self.quant = torch.quantization.QuantStub()
+        self.dequant = torch.quantization.DeQuantStub()
     def forward(self, x):
+        x = self.quant(x) # 在需要开启量化的层前后加入 量化层 和 反量化层
         x = self.features(x)
+        x = self.dequant(x)
         batch_size = x.size(0)
-
-        x = x.view(batch_size, 128, 4 ** 2)  ##
-
-        x = (torch.bmm(x, torch.transpose(x, 1, 2)) / 4 ** 2).view(batch_size, -1)  ##
-
+        x = x.view(batch_size, 128, 4 ** 2)
+        x = (torch.bmm(x, torch.transpose(x, 1, 2)) / 4 ** 2).view(batch_size, -1)
         x = torch.nn.functional.normalize(torch.sign(x) * torch.sqrt(torch.abs(x) + 1e-10))
-
+        x = self.quant(x)
         x = torch.sigmoid(self.classifiers(x))
+        x = self.dequant(x)
         return x.squeeze(1)
 
 
@@ -102,7 +98,6 @@ def train(dataloader, model, loss_fn, optimizer):
         output = model(image)
         cur_loss = loss_fn(output, y)
         cur_acc = torch.sum((y == output.round()).int()).float() / output.shape[0]
-
         # 反向传播
         optimizer.zero_grad()
         cur_loss.backward()
@@ -110,17 +105,16 @@ def train(dataloader, model, loss_fn, optimizer):
         loss += cur_loss.item()
         current += cur_acc.item()
         n = n + 1
-
     train_loss = loss / n
     train_acc = current / n
     print('train_loss: {:.4f}     train_acc: {:.4f}'.format(train_loss, train_acc))
     return train_loss, train_acc
 
-
 def val(dataloader, model, loss_fn):
     # 将模型转化为验证模型
     model.eval()
     loss, current, n = 0.0, 0.0, 0
+    start = time.time()
     with torch.no_grad():
         for batch, (x, y) in enumerate(dataloader):
             image, y = x.to(device), y.to(device).float()  # Ensure y is float
@@ -130,13 +124,12 @@ def val(dataloader, model, loss_fn):
             loss += cur_loss.item()
             current += cur_acc.item()
             n = n + 1
-
+    end = time.time()
+    print(f'time cost = {end - start}')
     val_loss = loss / n
     val_acc = current / n
     print('val_loss: {:.4f}     val_acc: {:.4f}'.format(val_loss, val_acc))
-    print()
     return val_loss, val_acc
-
 
 # 定义画图函数
 def matplot_loss(train_loss, val_loss):
@@ -149,7 +142,6 @@ def matplot_loss(train_loss, val_loss):
     plt.xlim(left=0)  # 设置x轴的最小值为0，如果epoch从1开始，可以去掉这行
     plt.title("loss")
     plt.show()
-
 
 def matplot_acc(train_acc, val_acc):
     plt.plot(train_acc, label='train_acc')
@@ -164,48 +156,44 @@ def matplot_acc(train_acc, val_acc):
 
 
 # 开始训练
-all_loss_train = [0.0, 0.0, 0.0, 0.0, 0.0]
-all_acc_train = [0.0, 0.0, 0.0, 0.0, 0.0]
-all_loss_val = [0.0, 0.0, 0.0, 0.0, 0.0]
-all_acc_val = [0.0, 0.0, 0.0, 0.0, 0.0]
-
-avg_loss_train = 0
-avg_acc_train = 0
-avg_loss_val = 0
-avg_acc_val = 0
+activation_sizes = [] # 存储中间激活值大小
 # lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
-
-for i in range(5):
-    # for i in range (1):
-    # model_Cnn = Cnn(input_shape).to(device)
-    # model = AlexNet(input_shape).to(device)
-    # model = DNNModel(input_shape).to(device)
+for i in range(1):
     model = Bilinear(input_shape).to(device)
+    
+    # 定义钩子函数并注册到 model 所有层
+    def hook_fn(module, input, output):
+        size_in_bytes = output.element_size() * output.numel() # 元素大小 * 元素总数
+        activation_sizes.append(size_in_bytes)
+    for name, layer in model.named_modules():
+        if isinstance(layer, (nn.Conv2d, nn.Linear, nn.ReLU)): # 选择需要监控的层
+            layer.register_forward_hook(hook_fn)
+    model.qconfig = get_default_qat_qconfig('fbgemm') # 配置 QAT
+    prepare_qat(model, inplace=True)
+
     # 定义一个损失函数
     loss_fn = nn.BCELoss()
     # 定义一个优化器
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     # 学习率每隔10轮变为原来的0.5
     # lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
-
     ROOT_TRAIN = TRAIN_DIRS[i]
     ROOT_TEST = VAL_DIRS[i]
     train_dataset = ImageFolder(ROOT_TRAIN, transform=train_transform)
     val_dataset = ImageFolder(ROOT_TEST, transform=val_transform)
-
     train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=6)
     val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=True, num_workers=6)
     loss_train = []
     acc_train = []
     loss_val = []
     acc_val = []
-
     epoch = 1
     min_acc = 0
     best_epoch = 0
     for t in range(epoch):
-        start = time.time()
         print(f"epoch{t + 1}\n-----------")
+        print(f"Memory usage: {psutil.Process(os.getpid()).memory_info().rss / 1024**2:.2f} MB")
+        start = time.time()
         train_loss, train_acc = train(train_dataloader, model, loss_fn, optimizer)
         val_loss, val_acc = val(val_dataloader, model, loss_fn)
 
@@ -213,17 +201,11 @@ for i in range(5):
         acc_train.append(train_acc)
         loss_val.append(val_loss)
         acc_val.append(val_acc)
-
         # 保存最好的模型权重
         if val_acc > min_acc:
             folder = 'save_model'
             if not os.path.exists(folder):
                 os.mkdir('save_model')
-            all_acc_val[i] = val_acc
-            all_loss_val[i] = val_loss
-            all_acc_train[i] = train_acc
-            all_loss_train[i] = train_loss
-
             min_acc = val_acc
             print(f"save best model, 第{t + 1}轮")
             best_epoch = t
@@ -234,18 +216,18 @@ for i in range(5):
         # lr_scheduler.step()
         end = time.time()
         print(end - start)
-    # all_time = end-start
-    # print("all_time",all_time)
+    # matplot_loss(loss_train, loss_val)
+    # matplot_acc(acc_train, acc_val)
+print('finish training')
 
-    avg_acc_val += all_acc_val[i]
-    avg_loss_val += all_loss_val[i]
-    avg_acc_train += all_acc_train[i]
-    avg_loss_train += all_loss_train[i]
-
-    matplot_loss(loss_train, loss_val)
-    matplot_acc(acc_train, acc_val)
-
-print("")
-print('avg_train_loss: {:.4f}     avg_train_acc: {:.4f}'.format(avg_loss_train / 5, avg_acc_train / 5))
-print('avg_val_loss: {:.4f}     avg_val_acc: {:.4f}'.format(avg_loss_val / 5, avg_acc_val / 5))
-print('Done!')
+print('start validation')
+# 测试不开启量化 model 最大中间激活值内存
+activation_sizes= []
+val_loss, val_acc = val(val_dataloader, model, loss_fn)
+print(f"model activation sizes: {max(activation_sizes) / (1024 ** 2):.4f} MB")
+# 测试开启量化后 model 最大中间激活值内存
+torch.backends.quantized.engine = 'fbgemm'
+quantized_model = torch.quantization.convert(model, inplace=False)
+activation_sizes= []
+val_loss, val_acc = val(val_dataloader, quantized_model, loss_fn)
+print(f"quantized_model activation sizes: {max(activation_sizes) / (1024 ** 2):.4f} MB")
